cba2c5ebc046f41e423a8612c70aadeb8a4ef3f5
Jordan Pavao <jordan@DESKTOP-6NKFFLI.corp.cervol.com>
2020-10-22 00:23:42 -0400

    Allowed for user to input subreddit name instead of copying entire subreddit link

--- index.js
+++ index.js
@@ -4,25 +4,40 @@ const cheerio = require('cheerio');
 const { prefix, token } = require('./config.json');
 const client = new Client();
 
+const api_path = "https://www.reddit.com/r/";
+const api_key = ".json";
 
 client.once('ready', () => {
     console.log('Ready!')
 })
 
 client.on('message', async message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    axios
+    axios.get(`${api_path}${command}${api_key}`).then(response => {
+        console.log(response)
+    });
+
+    /*const getData = () => {
+        axios.get(`${api_path}+${command}+${api_key}`).then(response => {
+            console.log(response)
+        });
+    }*/
+
+
+/*    axios
     .get(command)
     .then((response) => {
         console.log(response.data);
     })
     .catch((err) => {
         console.log(err);
-    });
+    });*/
 });
 
 
