c665429c0746e5fccf991efc5944c983742d2924
Jordan Pavao <jordan@DESKTOP-6NKFFLI.corp.cervol.com>
2020-10-22 18:10:09 -0400

    Discovered how to access individual elements for subreddit posts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { prefix, token } = require('./config.json');
 const client = new Client();
 
 const api_path = "https://www.reddit.com/r/";
-const api_key = ".json";
+const api_key = "/.json";
 
 client.once('ready', () => {
     console.log('Ready!')
@@ -13,32 +13,28 @@ client.once('ready', () => {
 
 client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    axios.get(`${api_path}${command}${api_key}`).then(response => {
-        console.log(response)
-    });
-
-    /*const getData = () => {
-        axios.get(`${api_path}+${command}+${api_key}`).then(response => {
-            console.log(response)
-        });
-    }*/
-
-
-/*    axios
-    .get(command)
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((err) => {
-        console.log(err);
-    });*/
+    if(command === 'help') {
+        //do something
+    } else if(command === 'next') {
+        //go next
+    } else if(command === 'prev') {
+        //go prev
+    } else {
+        axios.get(`${api_path}${command}${api_key}`)
+        .then(response => {
+            console.log(`${api_path}${command}${api_key}`)
+            //console.log(response.data.data.children.map(obj => obj.data))
+            const dataArr = response.data.data.children.map(obj => obj.data)
+            console.log(dataArr[0].permalink)
+        })
+        .catch((err) => {
+            console.log('ERR:', err)
+        })
+    }
 });
-
 
 client.login(token);
