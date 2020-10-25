d586ed8fee88652a04bf073a934def1911743174
Jordan Pavao <jordan@DESKTOP-6NKFFLI.corp.cervol.com>
2020-10-25 00:22:31 -0400

    Updated .gitignore

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const { Client, MessageEmbed } = require('discord.js');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 const { prefix, token } = require('./config.json');
+
+
 const client = new Client();
 
 const api_path = "https://www.reddit.com/r/";
+var subreddit = "";
 const api_key = "/.json";
+
+// Count Variable
+let count = 0;
 
 client.once('ready', () => {
     console.log('Ready!')
@@ -19,22 +24,37 @@ client.on('message', async message => {
 
     if(command === 'help') {
         //do something
-    } else if(command === 'next') {
+    } else if(command === 'n') {
+        count++;
+        console.log(count)
         //go next
     } else if(command === 'prev') {
         //go prev
     } else {
-        axios.get(`${api_path}${command}${api_key}`)
-        .then(response => {
-            console.log(`${api_path}${command}${api_key}`)
-            //console.log(response.data.data.children.map(obj => obj.data))
-            const dataArr = response.data.data.children.map(obj => obj.data)
-            console.log(dataArr[0].permalink)
-        })
-        .catch((err) => {
-            console.log('ERR:', err)
-        })
+        count = 0;
+        subreddit = command;
+        getAPI();
     }
 });
+
+// Calls Api with user requested subreddit
+function getAPI() {
+
+    fetch(`${api_path}${subreddit}${api_key}`).then(function(response) {
+        if(response.ok) {
+            console.log("made it");
+            return response.json();
+        } else {
+            throw new Error('Error');
+        }
+    }).then(function(json) {
+
+        embedData(json);
+        /*for (let i = 0; i < json.data.children.length; i++) {
+            console.log('https://www.reddit.com'+json.data.children[i].data.permalink);
+        }*/
+       
+    });
+}
 
 client.login(token);
