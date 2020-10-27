ea0963f6d245db8102b640767c2d80d0d705110c
Jordan Pavao <pavao64jordan@gmail.com>
2020-10-27 17:12:15 -0400

    Finished verification for displaying previous subreddit entries.

--- index.js
+++ index.js
@@ -21,14 +21,18 @@ client.once('ready', () => {
 client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    // Data Receiving Variable
+    let info;
+
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(command === 'help') {
         //do something
-    } else if(command === 'n') {
-        let info;
 
+    // Command 'n' represents a request to display the next entry
+    } else if(command === 'n') {
+        
         count++;
 
         getAPI(count).then(function(data) {
@@ -37,20 +41,30 @@ client.on('message', async message => {
         });
 
         console.log(count)
-        //go next
-    } else if(command === 'prev') {
-        let info;
 
-        count--;
-        //console.log(count)
+    // Command 'p' represents a request to display the previous entry
+    } else if(command === 'p') {
+        // Check if a valid subreddit exists
+        if(subreddit === "") {
+            message.channel.send("Please enter a valid subreddit before requesting previous subreddit entries");
+            return;
+        }
 
-        getAPI(count).then(function(data) {
-            info = data;
-            message.channel.send(info);
-        });
+        // Ensure that more than one subreddit entry has been displayed so that a previous entry may be output
+        else if(count <= 0) {
+            message.channel.send("The post above is the first entry in the subreddit");
+            return;
+        }
+        else {
+            count--;
+            //console.log(count)
+    
+            getAPI(count).then(function(data) {
+                info = data;
+                message.channel.send(info);
+            });
+        }
     } else {
-        let info;
-
         count = 0;
         subreddit = command;
 
@@ -64,6 +78,10 @@ client.on('message', async message => {
 
 // Calls Api with user requested subreddit
 function getAPI(index) {
+    if(subreddit === "") {
+        return "Please enter a valid subreddit";
+    }
+
     return fetch(`${api_path}${subreddit}${api_key}`).then(function(response) {
         return response.json();
     }).then(function(json) {
@@ -72,7 +90,10 @@ function getAPI(index) {
         }
         catch(err) {
             console.log(err)
-            return('Subreddit is private / does not exist');
+
+            // Setting subreddit = "" ensures that previous functions is not used on invalid subreddit
+            subreddit = "";
+            return('Subreddit is private / does not exist, Please enter a new subreddit!');
         }
     });
 }
